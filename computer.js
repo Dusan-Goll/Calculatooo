950d2f6f5c558d6af1f0e8fa7b434830680f9ee2
Dusan-Goll <d.goll@seznam.cz>
2022-12-20 11:39:29 +0100

    wrong behavior of minus sign - bug fixed

--- computer.js
+++ computer.js
@@ -37,8 +37,8 @@ class Calculator {
 
 // Chtelo by to funkci, co bude pri mackani buttonu kalkulacky
 // kontrolovat, zda-li prvni znak je cislo, nebo neco jineho
-// a v pripade ze nebude cislo, tak opravit input
-// a taky kdyz uzivatel zmackne vicekrat napr. 'plus', tak to
+// a v pripade ze nebude cislo, tak opravit input.
+// A taky kdyz uzivatel zmackne vicekrat napr. 'plus', tak to
 // prida do inputu (a zobrazi) jen to puvodni 'plus'.
 
 let someString = '850+63×12-30÷5+78-3×2÷2+140';
@@ -46,20 +46,25 @@ let someString = '850+63×12-30÷5+78-3×2÷2+140';
 
 /*** input string handling ***/
 // object-oriented -> transfer arguments to object properties
+
+
 class Computer extends Calculator {
+    constructor() {
+        super();
+        this.sings = ['×', '÷', '-', '+'];
+    }
     
     compute() {
-        let expression = this.convertToExpr(this.input.value),
-            checkSigns = ['×', '÷', '+', '-'];
+        let expression = this.convertToExpr(this.input.value);
     
-        for (let anSign of checkSigns) {
-            while (expression.includes(anSign)) {
-                expression = this.computePartOf(expression, anSign);
+        for (let sign of this.sings) {
+            while (expression.includes(sign)) {
+                expression = this.computePartOf(expression, sign);
             }
         }
-    
+        
         if (expression.length != 1) {
-            throw console.error('Array has more than one item.');
+            this.throwError();
         } else return expression[0];
     }
 
@@ -86,25 +91,41 @@ class Computer extends Calculator {
         return mathExpre;
     }
 
-    computePartOf(expr, sign) {
+    computePartOf(expr, sign) {  // 'operator' instead 'sign'
         let tempResult;
-    
-        while (expr.includes(sign)) {
-            let pos = expr.indexOf(sign);
-    
-            if (sign === '×') {
-                tempResult = expr[pos - 1] * expr[pos + 1];
-            } else if (sign === '÷') {
-                tempResult = expr[pos - 1] / expr[pos + 1];
-            } else if (sign === '+') {
-                tempResult = expr[pos - 1] + expr[pos + 1];
-            } else if (sign === '-') {
-                tempResult = expr[pos - 1] - expr[pos + 1];
+        
+        if (sign === '-') {
+            while (expr.includes(sign)) {
+                let pos = expr.indexOf(sign),
+                    negativeNum = -(expr[(pos + 1)]);
+
+                console.log('pred:', expr);
+                expr.splice(pos, 2, '+', negativeNum);
+                console.log('po', expr);
             }
-    
-            expr.splice((pos - 1), 3, tempResult);
+        } else {
+            while (expr.includes(sign)) {
+                let pos = expr.indexOf(sign);
+            
+                if (sign === '×') {
+                    tempResult = expr[pos - 1] * expr[pos + 1];
+                } else if (sign === '÷') {
+                    tempResult = expr[pos - 1] / expr[pos + 1]; 
+                } else if (sign === '+') {
+                    tempResult = expr[pos - 1] + expr[pos + 1];
+                }
+                console.log('pred', expr);
+                expr.splice((pos - 1), 3, tempResult);
+                console.log('po', expr);
+            }
         }
         return expr;
+    }
+
+    throwError() {
+        throw console.error(
+                'Array has either none or more than one item.'
+        );
     }
 }
 
